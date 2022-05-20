98d4bf2ea18959a15fd9527ef75ab94a59fc3c26
Fabiana Santos <fabianajmge@yahoo.com.br>
2022-05-20 12:26:59 -0300

    erro deploy

--- src/main/java/com/pederapido/pederapido/config/CorsConfig.java
+++ src/main/java/com/pederapido/pederapido/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 		.allowedOrigins("http://localhost:4200", "http://ec2-54-208-15-146.compute-1.amazonaws.com:8081",
-				"http://ec2-54-208-15-146.compute-1.amazonaws.com:80", "http://ec2-54-208-15-146.compute-1.amazonaws.com)
+				"http://ec2-54-208-15-146.compute-1.amazonaws.com:80", "http://ec2-54-208-15-146.compute-1.amazonaws.com")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
         .allowedHeaders("Authorization", "Content-Type", "Accept");
 	}
