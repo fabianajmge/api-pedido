dc79f62e87049795698675574b82ceecc901b403
Fabiana Santos <fabianajmge@yahoo.com.br>
2022-05-20 17:11:27 -0300

    cors filter

--- src/main/java/com/pederapido/pederapido/config/CorsConfig.java
+++ src/main/java/com/pederapido/pederapido/config/CorsConfig.java
@@ -9,12 +9,12 @@ public class CorsConfig implements WebMvcConfigurer {
 	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:4200", "http://ec2-54-208-15-146.compute-1.amazonaws.com:8081",
-				"http://ec2-54-208-15-146.compute-1.amazonaws.com:80", "http://ec2-54-208-15-146.compute-1.amazonaws.com",
-				"http://54.208.15.146", "http://54.208.15.146:8081")
-        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-        .allowedHeaders("Authorization", "Content-Type", "Accept");
+		 registry.addMapping("/**")
+         .allowedOrigins("http://localhost:4200", "http://ec2-100-26-208-1.compute-1.amazonaws.com:8081/",
+ 				"http://ec2-100-26-208-1.compute-1.amazonaws.com:8081/:80", "http://ec2-100-26-208-1.compute-1.amazonaws.com",
+ 				"http://100.26.208.1", "http://100.26.208.1:8081")
+         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+         .allowCredentials(false).maxAge(3600);
 	}
 
 }
