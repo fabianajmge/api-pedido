1d9bb1cda4d31f835f16709cbe1677e018e4a06b
Fabiana Santos <fabianajmge@yahoo.com.br>
2022-05-20 18:07:21 -0300

    cors

--- src/main/java/com/pederapido/pederapido/config/CorsConfig.java
+++ src/main/java/com/pederapido/pederapido/config/CorsConfig.java
@@ -4,17 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
+//@Configuration
+public class CorsConfig //implements WebMvcConfigurer 
+{
 	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		 registry.addMapping("/**")
-         .allowedOrigins("http://localhost:4200", "http://ec2-100-26-208-1.compute-1.amazonaws.com:8081/",
- 				"http://ec2-100-26-208-1.compute-1.amazonaws.com:8081/:80", "http://ec2-100-26-208-1.compute-1.amazonaws.com",
- 				"http://100.26.208.1", "http://100.26.208.1:8081")
-         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-         .allowCredentials(false).maxAge(3600);
-	}
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		 registry.addMapping("/**")
+//         .allowedOrigins("http://localhost:4200", "http://ec2-100-26-208-1.compute-1.amazonaws.com:8081/",
+// 				"http://ec2-100-26-208-1.compute-1.amazonaws.com:8081/:80", "http://ec2-100-26-208-1.compute-1.amazonaws.com",
+// 				"http://100.26.208.1", "http://100.26.208.1:8081")
+//         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+//         .allowCredentials(true);
+//	}
 
 }
